145fa7caeb54ad3ed14d5daadd176ac87e3aa598
WebFurre <109003493+WebFurre@users.noreply.github.com>
2025-10-04 14:03:24 +0300

    Add files via upload

---
+++ lab1/lab_1_2.py
@@ -0,0 +1,9 @@
+num_1=int(input("Введите первое число: "))
+num_2=int(input("Введите второе число: "))
+
+if num_1>num_2:
+    print(f"Большее число: {num_1}")
+elif num_2>num_1:
+    print(f"Большее число: {num_2}")
+else:
+    print("Числа равны.")
