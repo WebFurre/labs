c473ecccb252532de0d1405cd57daf030ba52559
WebFurre <109003493+WebFurre@users.noreply.github.com>
2025-10-04 14:07:04 +0300

    Delete lab1 directory

--- lab1/lab_1_1.py
+++
@@ -1,9 +0,0 @@
-num=float(input("Введите число: "))
-
-while num<1 or int(num)!=num:
-    print("число должно быть натуральным!!!!!!")
-    num=float(input("Введите число: "))
-num=int(num)
-
-for i in range(1,num+1):
-    print(i)
--- lab1/lab_1_2.py
+++
@@ -1,9 +0,0 @@
-num_1=int(input("Введите первое число: "))
-num_2=int(input("Введите второе число: "))
-
-if num_1>num_2:
-    print(f"Большее число: {num_1}")
-elif num_2>num_1:
-    print(f"Большее число: {num_2}")
-else:
-    print("Числа равны.")
